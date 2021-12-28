22960fb0324f5d021f036eb06cfd965e87f830c4
seanisoverhere <sean_choon@hotmail.com>
2021-12-29 02:03:36 +0800

    fix: fix trpc url gen

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,18 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { withTRPC } from '@trpc/next';
+import type { AppRouter } from '@/backend/router'; 
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
 }
-import { withTRPC } from '@trpc/next';
-import type { AppRouter } from '@/backend/router'; 
+
+function getBaseUrl() {
+  if (process.browser) return "";
+  if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}`;
+
+  return `http://localhost:${process.env.PORT ?? 3000}`;
+}
 
 export default withTRPC<AppRouter>({
   config({ ctx }) {
@@ -13,9 +20,7 @@ export default withTRPC<AppRouter>({
      * If you want to use SSR, you need to use the server's full URL
      * @link https://trpc.io/docs/ssr
      */
-    const url = process.env.VERCEL_URL
-      ? `https://${process.env.VERCEL_URL}/api/trpc`
-      : 'http://localhost:3000/api/trpc';
+    const url = `${getBaseUrl()}/api/trpc`;
 
     return {
       url,
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,8 +4,11 @@ import { useState } from "react";
 import { trpc } from "@/utils/trpc";
 import { getOptionsForVote } from "@/utils/getRandomPokemon";
 
+const btn =
+  "inline-flex items-center px-4 py-1 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500";
+
 const Home: NextPage = () => {
-  const [ids, updateids] = useState(getOptionsForVote());
+  const [ids, updateIds] = useState(() => getOptionsForVote());
 
   const [first, second] = ids;
 
@@ -25,6 +28,11 @@ const Home: NextPage = () => {
 
   if (firstPokemon.isLoading || secondPokemon.isLoading) return null;
 
+  const voteForHeaviest = (selected: number) => {
+    // fire mutation to persist changes
+    updateIds(getOptionsForVote());
+  };
+
   return (
     <div className="h-screen w-screen flex flex-col justify-center items-center">
       <Head>
@@ -35,14 +43,26 @@ const Home: NextPage = () => {
 
       <div className="text-2xl text-center">Which Pokémon is Heavier?</div>
       <div className="border rounded mt-6 p-8 flex justify-between items-center max-w-2xl">
-        <div className="w-64 h-64 flex flex-col">
-          <img src={firstPokemon.data?.sprites.front_default!} className="w-full" />
-          <div className="text-xl text-center capitalize mt-[-2rem]">{firstPokemon.data?.name}</div>
+        <div className="w-64 h-64 flex flex-col items-center">
+          <img
+            src={firstPokemon.data?.sprites.front_default!}
+            className="w-full"
+          />
+          <div className="text-xl text-center capitalize mt-[-2rem]">
+            {firstPokemon.data?.name}
+          </div>
+          <button className={btn} onClick={() => voteForHeaviest(first)}>Heavier</button>
         </div>
         <div className="p-8">VS</div>
-        <div className="w-64 h-64 flex flex-col">
-          <img src={secondPokemon.data?.sprites.front_default!} className="w-full" />
-          <div className="text-xl text-center capitalize mt-[-2rem]">{secondPokemon.data?.name}</div>
+        <div className="w-64 h-64 flex flex-col items-center">
+          <img
+            src={secondPokemon.data?.sprites.front_default!}
+            className="w-full"
+          />
+          <div className="text-xl text-center capitalize mt-[-2rem]">
+            {secondPokemon.data?.name}
+          </div>
+          <button className={btn} onClick={() => voteForHeaviest(second)}>Heavier</button>
         </div>
       </div>
     </div>
