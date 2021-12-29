e77083c0ed28bda9ab47cd30f48e5b15c436ef74
seanisoverhere <sean_choon@hotmail.com>
2021-12-29 12:58:02 +0800

    feat: add primsa connection with planetscale

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,12 @@
 import type { NextPage } from "next";
 import Head from "next/head";
-import { useState } from "react";
+import React, { useState } from "react";
 import { trpc } from "@/utils/trpc";
 import { getOptionsForVote } from "@/utils/getRandomPokemon";
+import type { inferQueryResponse } from "./api/trpc/[trpc]";
 
 const btn =
-  "inline-flex items-center px-4 py-1 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500";
+  "inline-flex items-center px-4 py-1 mt-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500";
 
 const Home: NextPage = () => {
   const [ids, updateIds] = useState(() => getOptionsForVote());
@@ -26,8 +27,6 @@ const Home: NextPage = () => {
     },
   ]);
 
-  if (firstPokemon.isLoading || secondPokemon.isLoading) return null;
-
   const voteForHeaviest = (selected: number) => {
     // fire mutation to persist changes
     updateIds(getOptionsForVote());
@@ -43,30 +42,47 @@ const Home: NextPage = () => {
 
       <div className="text-2xl text-center">Which Pokémon is Heavier?</div>
       <div className="border rounded mt-6 p-8 flex justify-between items-center max-w-2xl">
-        <div className="w-64 h-64 flex flex-col items-center">
-          <img
-            src={firstPokemon.data?.sprites.front_default!}
-            className="w-full"
-          />
-          <div className="text-xl text-center capitalize mt-[-2rem]">
-            {firstPokemon.data?.name}
-          </div>
-          <button className={btn} onClick={() => voteForHeaviest(first)}>Heavier</button>
-        </div>
-        <div className="p-8">VS</div>
-        <div className="w-64 h-64 flex flex-col items-center">
-          <img
-            src={secondPokemon.data?.sprites.front_default!}
-            className="w-full"
-          />
-          <div className="text-xl text-center capitalize mt-[-2rem]">
-            {secondPokemon.data?.name}
-          </div>
-          <button className={btn} onClick={() => voteForHeaviest(second)}>Heavier</button>
-        </div>
+        {!firstPokemon.isLoading &&
+          firstPokemon.data &&
+          !secondPokemon.isLoading &&
+          secondPokemon.data && (
+            <>
+              <PokemonListing
+                pokemon={firstPokemon.data}
+                vote={() => voteForHeaviest(first)}
+              />
+              <div className="p-8">VS</div>
+              <PokemonListing
+                pokemon={secondPokemon.data}
+                vote={() => voteForHeaviest(second)}
+              />
+            </>
+          )}
       </div>
     </div>
   );
 };
 
 export default Home;
+
+type PokemonFromServer = inferQueryResponse<"get-pokemon-by-id">;
+
+const PokemonListing: React.FC<{
+  pokemon: PokemonFromServer;
+  vote: () => void;
+}> = (props) => {
+  return (
+    <div className="flex flex-col items-center">
+      <img
+        src={props.pokemon.sprites.front_default!}
+        className="w-64 h-64 "
+      />
+      <div className="text-xl text-center capitalize mt-[-2rem]">
+        {props.pokemon.name}
+      </div>
+      <button className={btn} onClick={() => props.vote()}>
+        Heavier
+      </button>
+    </div>
+  );
+};
